4192657065ae3dc7318e07391310620f92927f69
FORGE096 <forge1803@gmail.com>
2026-05-17 23:42:26 +0330

    repo created

---
+++ models/users.mjs
@@ -0,0 +1,18 @@
+let users = [
+    { id: "0", name: "Mohammad", skill: "Flutter" },
+    { id: "1", name: "Ali", skill: "Flutter" },
+    { id: "2", name: "Emad", skill: "Flutter" },
+    { id: "3", name: "Reza", skill: "Flutter" },
+    { id: "4", name: "Mehrdad", skill: "Flutter" },
+    { id: "5", name: "Fatemeh", skill: "Flutter" },
+    { id: "6", name: "Zahra", skill: "Flutter" },
+    { id: "7", name: "Amir", skill: "Flutter" },
+    { id: "8", name: "Hassan", skill: "Flutter" },
+    { id: "9", name: "Poya", skill: "Flutter" },
+    { id: "10", name: "Sohrab", skill: "Flutter" },
+    { id: "11", name: "Hossein", skill: "Flutter" },
+];
+
+export {
+    users,
+}
