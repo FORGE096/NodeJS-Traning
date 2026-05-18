df03ab6a86ce4405842e871a66f343dc5c807c0e
FORGE096 <forge1803@gmail.com>
2026-05-18 17:03:16 +0330

    added a controller for users requests for more controll and readability of code

---
+++ controllers/users_controller.mjs
@@ -0,0 +1,92 @@
+import { users } from "../models/users.mjs";
+
+function getAllUsers(request, response) {
+    response.send({
+        CODE: response.statusCode,
+        MESSAGE: "Users List Fetched",
+        USERS: users,
+    });
+}
+
+function getUserByID(request, response) {
+    const id = request.params.id;
+    const user = users[id]
+    if (user) {
+        response.send({
+            CODE: response.statusCode,
+            MESSAGE: `User With ID ${id} Has Been Fetched!`,
+            USER: users[id],
+        });
+        return;
+    } else {
+        response.status(404).json({
+            CODE: response.statusCode,
+            MESSAGE: `User Not Found With ID ${id}`,
+        });
+        return;
+    }
+}
+
+function postNewUser(request, response) {
+    const newUser = request.body;
+    users.push(newUser);
+    response.status(201).json({
+        CODE: response.statusCode,
+        MESSAGE: "User Created",
+        USER: newUser,
+        USERS: users,
+    });
+}
+
+function updateUser(request, response) {
+    var user = users[request.params.id];
+
+    if (!user) {
+        return response.status(404).json({
+            CODE: response.statusCode,
+            MESSAGE: `User With ID ${user} Not Found`,
+        });
+    }
+    if (!request.body.name) {
+        return response.status(404).json({
+            CODE: response.statusCode,
+            MESSAGE: `User With ID ${user} Not Found`,
+        });
+    }
+
+    user.name = request.body.name;
+    user.skill = request.body.skill ?? "None";
+    response.send({
+        CODE: response.statusCode,
+        user: user,
+        USERS: users,
+    });
+}
+
+function deleteUser(request, response) {
+    const user = users.find(user => user.id === parseInt(request.params.id));
+
+    if (!user) {
+        console.log(user);
+        return response.status(404).json({
+            CODE: response.statusCode,
+            MESSAGE: "User Not Found!",
+        });
+    }
+
+    const deletedUser = users.splice(user, 1);
+
+    response.send({
+        CODE: response.statusCode,
+        MESSAGE: "Done! User Deleted!",
+        USER: deletedUser,
+    });
+}
+
+export {
+    getAllUsers,
+    getUserByID,
+    postNewUser,
+    updateUser,
+    deleteUser,
+};
--- main.mjs
+++ main.mjs
@@ -1,5 +1,6 @@
 import { users } from "./models/users.mjs";
 import express from "express";
+import { getAllUsers, getUserByID, postNewUser, updateUser, deleteUser } from "./controllers/users_controller.mjs";
 
 const app = express();
 const port = 6500;
@@ -18,88 +19,13 @@ app.get("/", (request, response) => {
     });
 });
 
-app.get("/users", (request, response) => {
-    response.send({
-        CODE: response.statusCode,
-        MESSAGE: "Users List Fetched",
-        USERS: users,
-    });
-});
+app.get("/users/:id", getUserByID);
 
-app.get("/users/:id", (request, response) => {
-    const id = request.params.id;
-    const user = users[id]
-    if (user) {
-        response.send({
-            CODE: response.statusCode,
-            MESSAGE: `User With ID ${id} Has Been Fetched!`,
-            USER: users[id],
-        });
-        return;
-    } else {
-        response.status(404).json({
-            CODE: response.statusCode,
-            MESSAGE: `User Not Found With ID ${id}`,
-        });
-        return;
-    }
-});
+app.post("/users", postNewUser);
 
-app.post("/users", (request, response) => {
-    const newUser = request.body;
-    users.push(newUser);
-    response.status(201).json({
-        CODE: response.statusCode,
-        MESSAGE: "User Created",
-        USER: newUser,
-        USERS: users,
-    });
-});
+app.put("/users/:id", updateUser);
 
-app.put("/users/:id", (request, response) => {
-    var user = users[request.params.id];
-
-    if (!user) {
-        return response.status(404).json({
-            CODE: response.statusCode,
-            MESSAGE: `User With ID ${user} Not Found`,
-        });
-    }
-    if (!request.body.name) {
-        return response.status(404).json({
-            CODE: response.statusCode,
-            MESSAGE: `User With ID ${user} Not Found`,
-        });
-    }
-
-    user.name = request.body.name;
-    user.skill = request.body.skill ?? "None";
-    response.send({
-        CODE: response.statusCode,
-        user: user,
-        USERS: users,
-    });
-});
-
-app.delete("/users/:id", (request, response) => {
-    const user = users.find(user => user.id === parseInt(request.params.id));
-
-    if (!user) {
-        console.log(user);
-        return response.status(404).json({
-            CODE: response.statusCode,
-            MESSAGE: "User Not Found!",
-        });
-    }
-
-    const deletedUser = users.splice(user, 1);
-
-    response.send({
-        CODE: response.statusCode,
-        MESSAGE: "Done! User Deleted!",
-        USER: deletedUser,
-    });
-});
+app.delete("/users/:id", deleteUser);
 
 app.listen(port, () => {
     console.log(`Lisening To ${port}`);
